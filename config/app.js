843835b50d47b0a0f97284a52720e373a9d22352
snupa <bunta.adrian@gmail.com>
2016-12-27 08:01:15 +0200

    updated app config

--- config/app.js
+++ config/app.js
@@ -7,13 +7,23 @@ module.exports = {
     debug: false
   },
   "store.redis": {
-    "host": process.env.REDIS_HOST || "localhost",
-    "port": process.env.REDIS_PORT || 6379,
-    "password": process.env.REDIS_PASSWORD || null
+    "prefix": "t-discovery",
+    "namespace": "disc",
+    "host": "localhost",
+    "port": 6379
   },
-  "store.redis.prefix": "t-discovery",
-  "config.maxSize": 10000 // max number of chars for the config actions
+  "config.maxSize": 50000 // max number of chars for the config actions
 };
+
+if(process.env.REDIS_HOST) {
+  module.exports['store.redis'].host = process.env.REDIS_HOST;
+}
+if(process.env.REDIS_PORT) {
+  module.exports['store.redis'].port = parseInt(process.env.REDIS_PORT);
+}
+if(process.env.REDIS_PASS || process.env.REDIS_PASSWORD) {
+  module.exports['store.redis'].password = process.env.REDIS_PASS || process.env.REDIS_PASSWORD;
+}
 
 if (process.env.SCONFIG_KEY) {
   thorin
